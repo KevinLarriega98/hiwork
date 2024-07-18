783142cf0e33a68a582689caaccf0a7a172e2913
marcgonzaleztarrio <marcgonzaleztarrio@gmail.com>
2024-07-18 11:56:21 +0200

    types of profiles

---
+++ src/types/ong.ts
@@ -0,0 +1,12 @@
+export interface OngState {
+    id: string;
+    name: string;
+    mission: string;
+    description: string;
+    location: string;
+    website: string;
+    contactEmail: string;
+    projects: string[];
+    createdAt: string;
+    updatedAt: string;
+}
