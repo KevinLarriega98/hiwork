796a2a3b2fb162ec7b4595dbfb10d7ba87aadda4
juanjo-0210 <juan.jose.mateo.0210@gmail.com>
2024-07-19 10:18:40 +0200

    se añade componente RegisterInput

--- src/screens/profile/OngRegisterScreen.tsx
+++ src/screens/profile/OngRegisterScreen.tsx
@@ -1,10 +1,46 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, { useState } from 'react';
+import {Pressable, Text, View} from 'react-native';
+import { RegisterInput } from '../../components/inputs';
+import { useNavigation } from '@react-navigation/native';
 
 export const OngRegisterScreen = () => {
+  const navigation = useNavigation()
+  const [name, setName] = useState("")
+  const [lastName, setLastName] = useState("")
+
   return (
     <View style={{flex:1}}>
-      <Text> OngRegisterScreen</Text>
+      
+
+      <RegisterInput 
+        placeHolder='Nombre'
+        label='Nombre'
+        onChange={setName}
+        value={name}
+      />
+
+      <RegisterInput 
+        placeHolder='Apellido'
+        label='Apellido'
+        onChange={setLastName}
+        value={lastName}
+      />
+
+      <RegisterInput 
+        placeHolder='Email'
+        label='Correo Electronico'
+        onChange={setLastName}
+        value={lastName}
+      />
+
+
+      <Pressable
+        onPress={() => navigation.goBack()}
+      >
+        <Text>Iniciar session</Text>
+      </Pressable>
     </View>
   );
 };
+
+
--- src/screens/profile/UserRegisterScreen.tsx
+++ src/screens/profile/UserRegisterScreen.tsx
@@ -1,12 +1,40 @@
 import { useNavigation } from '@react-navigation/native';
-import React from 'react';
+import React, { useState } from 'react';
 import {Pressable, Text, View} from 'react-native';
+import { RegisterInput } from '../../components/inputs';
 
 export const UserRegisterScreen = () => {
   const navigation = useNavigation()
+  const [name, setName] = useState("")
+  const [lastName, setLastName] = useState("")
+
+  
   return (
     <View style={{flex:1}}>
-      <Text> UserRegisterScreen</Text>
+      
+
+      <RegisterInput 
+        placeHolder='Nombre'
+        label='Nombre'
+        onChange={setName}
+        value={name}
+      />
+
+      <RegisterInput 
+        placeHolder='Apellido'
+        label='Apellido'
+        onChange={setLastName}
+        value={lastName}
+      />
+
+      <RegisterInput 
+        placeHolder='Email'
+        label='Correo Electronico'
+        onChange={setLastName}
+        value={lastName}
+      />
+
+      
       <Pressable
         onPress={() => navigation.goBack()}
       >
